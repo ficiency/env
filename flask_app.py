e76a451085b15f4a5b653ca6c82712cf5e5d4477
ficiency <ficiencybusiness@gmail.com>
2022-04-08 13:29:27 -0500

    test

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from googletrans import Translator
 from processing import calculate_mode
 import time
 import datetime, pytz
-
+print("Hola")
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["SECRET_KEY"] = "asdzxcfghvbnasfgraarasd"
